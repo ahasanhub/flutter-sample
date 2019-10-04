864090d8f167872a3826c03930370b94d026cb45
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-10-04 16:25:14 +0600

    update from

--- flutter_infinite_list/lib/src/bloc/post_state.dart
+++ flutter_infinite_list/lib/src/bloc/post_state.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_infinite_list/src/bloc/post_event.dart';
+import 'package:flutter_infinite_list/src/models/models.dart';
 
 abstract class PostState extends Equatable {
   PostState([List props = const []]) : super(props);
@@ -10,7 +11,21 @@ class PostUninitilized extends PostState {
   String toString() => 'PostUninitialize';
 }
 
-class PostLoaded extends PostState {}
+class PostLoaded extends PostState {
+  final List<Post> posts;
+  final bool hasReachedMax;
+
+  PostLoaded({this.posts, this.hasReachedMax}) : super([posts, hasReachedMax]);
+  PostLoaded copyWith({List<Post> posts, bool hasReachedMax}) {
+    return PostLoaded(
+        posts: posts ?? this.posts,
+        hasReachedMax: hasReachedMax ?? this.hasReachedMax);
+  }
+
+  @override
+  String toString() =>
+      'PostLoaded{post:${posts.length},hasReachedMax:$hasReachedMax}';
+}
 
 class PostError extends PostEvent {
   @override
