91894c8fec987f678111a5b723e8e6fe99d73687
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-10-05 22:17:51 +0600

    update some code

--- flutter_infinite_list/lib/src/app.dart
+++ flutter_infinite_list/lib/src/app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_infinite_list/src/bloc/bloc.dart';
+import 'package:flutter_infinite_list/src/pages/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/src/bloc/post_bloc.dart';
 
 class App extends StatelessWidget {
   @override
@@ -7,12 +12,12 @@ class App extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Posts'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        body: BlocProvider(
+          builder: (context) =>
+              PostBloc(httpClient: http.Client())..dispatch(Fetch()),
+          child: HomePage(),
         ),
       ),
     );
---
+++ flutter_infinite_list/lib/src/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/src/bloc/bloc.dart';
+
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  PostBloc _postBloc;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBloc = BlocProvider.of<PostBloc>(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _postBloc.dispatch(Fetch());
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
