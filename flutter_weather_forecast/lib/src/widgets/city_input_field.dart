151b6d270c12f92e45cc692d849076c64dd063a7
Ahasan Habib <shobuz.cse03@gmail.com>
2019-10-14 18:00:24 +0600

    update weather app

---
+++ flutter_weather_forecast/lib/src/widgets/city_input_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CityInputField extends StatefulWidget {
+  CityInputField({Key key}) : super(key: key);
+
+  _CityInputFieldState createState() => _CityInputFieldState();
+}
+
+class _CityInputFieldState extends State<CityInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
