74243a5cbef7a18032218d7e873d23fd4fb6b2d0
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-10-14 23:00:21 +0600

    update weather app

--- flutter_weather_forecast/lib/src/pages/weather_page.dart
+++ flutter_weather_forecast/lib/src/pages/weather_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:flutter_weather_forecast/src/bloc/bloc.dart';
+import 'package:flutter_weather_forecast/src/bloc/weather_bloc.dart';
+
+import 'package:flutter_weather_forecast/src/widgets/city_input_field.dart';
+
+import '../models/weather.dart';
 
 class WeatherPage extends StatefulWidget {
   WeatherPage({Key key}) : super(key: key);
@@ -7,32 +15,71 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final weatherBloc = WeatherBloc();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Fake Weather App'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 16),
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              "City Name",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              "35 °C",
-              style: TextStyle(fontSize: 80),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Fake Weather App'),
         ),
-      ),
+        body: BlocProvider(
+          builder: (context) => weatherBloc,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            alignment: Alignment.center,
+            child: BlocBuilder(
+              bloc: weatherBloc,
+              builder: (BuildContext context, WeatherState state) {
+                // Changing the UI based on the current state
+                if (state is WeatherInitial) {
+                  return _buildInitialInput();
+                } else if (state is WeatherLoading) {
+                  return _buildLoading();
+                } else if (state is WeatherLoaded) {
+                  return _buildColumnWithData(state.weather);
+                }
+                return _buildLoading();
+              },
+            ),
+          ),
+        ));
+  }
+
+  Widget _buildInitialInput() {
+    return Center(
+      child: CityInputField(),
     );
+  }
+
+  Widget _buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Column _buildColumnWithData(Weather weather) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          weather.cityName,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          '${weather.tempreture.toStringAsFixed(1)} °C',
+          style: TextStyle(fontSize: 80),
+        ),
+        CityInputField()
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    weatherBloc.dispose();
   }
 }
--- flutter_weather_forecast/lib/src/widgets/city_input_field.dart
+++ flutter_weather_forecast/lib/src/widgets/city_input_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/bloc.dart';
 
 class CityInputField extends StatefulWidget {
   CityInputField({Key key}) : super(key: key);
@@ -9,8 +12,24 @@ class CityInputField extends StatefulWidget {
 class _CityInputFieldState extends State<CityInputField> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (String name) {
+          final weatherBloc = BlocProvider.of<WeatherBloc>(context);
+          weatherBloc.dispatch(GetWeather(cityName: name));
+        },
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: 'Enter a city',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: Icon(Icons.search),
+        ),
+      ),
     );
+  }
+
+  void submitCityName(String cityName) {
+    //Get the bloc using the BlocProvider
   }
 }
