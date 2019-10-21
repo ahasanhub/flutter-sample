dbe524bd410fc7b6f8fe36ce170af847bdb1a958
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-10-21 22:37:36 +0600

    update from home

---
+++ flutter_weather/lib/src/widgets/city_selection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CitySelection extends StatefulWidget {
+  @override
+  _CitySelectionState createState() => _CitySelectionState();
+}
+
+class _CitySelectionState extends State<CitySelection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
---
+++ flutter_weather/lib/src/widgets/weather.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/src/bloc/bloc.dart';
+import 'package:flutter_weather/src/widgets/widgets.dart';
+
+class Weather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CitySelection()));
+              if (city != null) {
+                BlocProvider.of<WeatherBloc>(context)
+                    .add(FetchWeather(city: city));
+              }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder(
+          builder: (context, state) {
+            if (state is WeatherEmpty) {
+              return Center(
+                child: Text('Please select a location'),
+              );
+            }
+            if (state is WeatherLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return null;
+          },
+        ),
+      ),
+    );
+  }
+}
