079bb9fbceabcd17ad3025ac919344ec07859260
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-10-27 22:19:25 +0600

    update from home

--- flutter_weather/lib/src/app.dart
+++ flutter_weather/lib/src/app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather/src/bloc/bloc.dart';
 import 'package:flutter_weather/src/repositories/weather_repository.dart';
 
+import 'widgets/weather.dart';
+
 class App extends StatelessWidget {
   final WeatherRepository weatherRepository;
   App({Key key, this.weatherRepository})
@@ -13,10 +15,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Weather',
       home: BlocProvider(
-          builder: (context) =>
-              WeatherBloc(weatherRepository: weatherRepository),
-          child: null //Weather(),
-          ),
+        builder: (context) => WeatherBloc(weatherRepository: weatherRepository),
+        child: Weather(),
+      ),
     );
   }
 }
